d717ca1b845b4b003fe7caac36590ff26e483cfd
BaoPG4 <BaoPG4>
2024-05-07 10:34:58 +0700

    Exe4_Express Serve Static Files

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-const express = require('express'),
-     http = require('http');
-
+const express = require('express'), http = require('http');
+const morgan = require('morgan');
 const hostname = 'localhost';
 const port = 3000;
 
@@ -13,6 +12,9 @@ app.use((req, res, next) => {
   res.end('<html><body><h1>This is an Express Server</h1></body></html>');
 
 });
+
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
 
 const server = http.createServer(app);
 
